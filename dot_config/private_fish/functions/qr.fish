cb1d971e4f0d9652dea2d144eb1d3f87351c454e
Pantos <pantos+github@akk.org>
2024-05-04 14:02:20 +0200

    fish: display qr as utf-8 instead of image

--- dot_config/private_fish/functions/qr.fish
+++ dot_config/private_fish/functions/qr.fish
@@ -9,28 +9,16 @@ function qr
             echo "wl-paste not found. (Package wl-clipboard)" 1>&2
             return 1
         end
-        if not command -v swayimg > dev/null 2>&1;
-            echo "swayimg not found." 1>&2
-            return 1
-        end
 
         set clip_content "$(wl-paste)"
-        set imgprog "swayimg"
     else
         if not command -v xclip > /dev/null 2>&1;
             echo "xlip not found." 1>&2
             return 1
         end
-        if not command -v viewnior > /dev/null 2>&1;
-            echo "viewnior not found." 1>&2
-            return 1
-        end
 
         set clip_content "$(xclip -o)"
-        set imgprog "viewnior"
     end
 
-    set qrfile "/tmp/qr.png"
-    command qrencode -s 8 -o "$qrfile" "$clip_content"
-    command "$imgprog" "$qrfile"
+    command qrencode -s 8 -o -t utf8 "$clip_content"
 end
