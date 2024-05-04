0e224aa54cfab07743ca43f54fe5231755d64074
Pantos <pantos+github@akk.org>
2024-05-04 14:02:19 +0200

    fish: enable hostname-dependant configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,9 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
+# include hostname-dependent config
+source $HOME/.config/fish/conf.$hostname.d/*.fish
