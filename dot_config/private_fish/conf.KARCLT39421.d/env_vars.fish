2af8c2e76997f1f23d4465917c579eacff928d8f
Pantos <pantos+github@akk.org>
2024-05-04 14:02:20 +0200

    fish: change KARCLT jumphost

--- dot_config/private_fish/conf.KARCLT39421.d/env_vars.fish
+++ dot_config/private_fish/conf.KARCLT39421.d/env_vars.fish
@@ -1,3 +1,3 @@
-set -gx HO_JUMPHOST 'jh-anham-dev'
+# set -gx HO_JUMPHOST 'jh-anham-dev'
 # set -gx HO_JUMPHOST 'jh-kar-appteam'
-# set -gx HO_JUMPHOST 'jh-ptp-webserver'
+set -gx HO_JUMPHOST 'jh-ptp-webserver'
