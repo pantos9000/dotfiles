3573e3431e665bf8820fe0ca14a62b539ef7e785
Pantos <pantos+github@akk.org>
2024-05-04 14:02:21 +0200

    nvim: remove unused comment shortcuts

--- dot_config/nvim/lua/configs/config_comment.lua
+++ dot_config/nvim/lua/configs/config_comment.lua
@@ -2,31 +2,24 @@ local M = function()
     require('Comment').setup({
         sticky = true,
         toggler = {
-            line = '<C-c><C-c>', -- toggle with line comment
-            block = '<C-c><C-b>', -- toggle with block comment
+            line = '<C-c>',  -- toggle with line comment
+            block = '<C-b>', -- toggle with block comment
         },
         opleader = {
-            line = '<C-c><C-c>', -- toggle with line comment
-            block = '<C-c><C-b>', -- toggle with block comment
+            line = '<C-c>',  -- toggle with line comment
+            block = '<C-b>', -- toggle with block comment
         },
-        extra = {
-            above = '<C-c><Up>', -- add comment in line above
-            below = '<C-c><Down>', -- add comment in line below
-            eol = '<C-c><Right>', -- add comment at end of line
-        },
+        mappings = {
+            basic = true,
+            extra = false,
+        }
     })
 
     -- also allow shortcuts in insert mode
-    vim.keymap.set('i', '<C-c><C-c>', '<C-o><C-c><C-c>',
+    vim.keymap.set('i', '<C-c>', '<C-o><C-c>',
         { remap = true, desc = 'toggle comment line (insert mode)' })
-    vim.keymap.set('i', '<C-c><C-b>', '<C-o><C-c><C-b>',
+    vim.keymap.set('i', '<C-b>', '<C-o><C-b>',
         { remap = true, desc = 'toggle comment block (insert mode)' })
-    vim.keymap.set('i', '<C-c><Up>', '<C-o><C-c><Up>',
-        { remap = true, desc = 'add comment above (insert mode)' })
-    vim.keymap.set('i', '<C-c><Down>', '<C-o><C-c><Down>',
-        { remap = true, desc = 'add comment below (insert mode)' })
-    vim.keymap.set('i', '<C-c><Right>', '<C-o><C-c><Right>',
-        { remap = true, desc = 'add comment at end of line (insert mode)' })
 end
 
 return M
