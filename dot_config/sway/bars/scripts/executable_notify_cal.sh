1d06d726bdae6a391b9628836fcf360915b91f2b
Pantos <pantos+github@akk.org>
2024-05-04 14:02:20 +0200

    sway: delete old py3status config

--- dot_config/sway/bars/scripts/executable_notify_cal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-day=$(date | cut -d ' ' -f2)
-day=${day%?}
-cal | sed "s/\b\($day\)\b/<b><span color='lime'>\1<\/span><\/b>/g"
-
--- dot_config/sway/bars/scripts/old/executable_volume_down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/bin/amixer set Master 10%- &> /dev/null
-~/.i3/scripts/volume_bar.sh & &> /dev/null
-
