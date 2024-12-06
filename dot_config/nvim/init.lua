6c665161557b952491b9a03a12366daede6614b0
Pantos <pantos+github@akk.org>
2024-12-06 16:14:56 +0100

    nvim: remove old todos

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,10 +2,3 @@ require('plugins')
 require('mappings')
 require('settings')
 
-
--- TODO debugging
---      https://alpha2phi.medium.com/setting-up-neovim-for-rust-debugging-termdebug-and-vimspector-df749e1ba47c
---      https://github.com/mfussenegger/nvim-dap
---      https://github.com/rcarriga/nvim-dap-ui
---      https://github.com/jay-babu/mason-nvim-dap.nvim
---      rust-tools failed: :(
