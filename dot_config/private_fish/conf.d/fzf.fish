3f95dbb826014c1555af8994a02a60ba374ef41e
Pantos <pantos+github@akk.org>
2024-12-19 10:05:09 +0100

    fish: use delta for diffs

--- dot_config/private_fish/conf.d/fzf.fish
+++ dot_config/private_fish/conf.d/fzf.fish
@@ -26,7 +26,3 @@ function _fzf_uninstall --on-event fzf_uninstall
     echo "You may need to manually remove fzf_configure_bindings from your config.fish if you were using custom key bindings."
     set_color normal
 end
-
-# use delta for diffs
-set fzf_diff_highlighter delta --paging=never --width=20
-
