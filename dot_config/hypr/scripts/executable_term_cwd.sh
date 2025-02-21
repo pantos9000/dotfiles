4d02bc753446b0a722e3234bda760c2d4984ffed
Pantos <pantos+github@akk.org>
2025-02-21 09:59:24 +0100

    hypr: remove obsolete term_cwd script

--- dot_config/hypr/scripts/executable_term_cwd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-set -euo pipefail
-
-pid=$(hyprctl activewindow | grep pid | cut -d ' ' -f2)
-ppid=$(pgrep --newest --parent "$pid")
-cwd="$(readlink "/proc/$ppid/cwd" || echo "$HOME")"
-
-alacritty --working-directory "$cwd"
