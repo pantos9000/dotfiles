3657dc9e27a44bbfa230ddc3eac48979d7a13163
Pantos <pantos+github@akk.org>
2025-02-23 01:11:05 +0100

    nvim: spider: also use old movement for 'delete'

--- dot_config/nvim/lua/plugins/spider.lua
+++ dot_config/nvim/lua/plugins/spider.lua
@@ -7,6 +7,7 @@ return {
     vim.keymap.set({ "n", "o", "x" }, "b", function() require("spider").motion "b" end, { desc = "Spider-b" })
     vim.keymap.set({ "n", "o", "x" }, "q", function() require("spider").motion "b" end, { desc = "Spider-b" })
     vim.keymap.set("n", "cw", "c<cmd>lua require('spider').motion('e')<CR>")
+    vim.keymap.set("n", "dw", "d<cmd>lua require('spider').motion('e')<CR>")
     vim.keymap.set("n", "vw", "v<cmd>lua require('spider').motion('e')<CR>")
     vim.keymap.set("n", "yw", "y<cmd>lua require('spider').motion('e')<CR>")
   end,
