d48cfb5bef84fb692f7b2c95085666e61beeda4d
Pantos <pantos+github@akk.org>
2025-02-26 15:08:07 +0100

    nvim: remove polish customization that is not executed anyway

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -4,9 +4,6 @@ if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- set color of whitespaces
-vim.cmd([[highlight Whitespace ctermfg=darkcyan guifg=darkcyan]])
-
 -- Set up custom filetypes
 vim.filetype.add {
   extension = {
