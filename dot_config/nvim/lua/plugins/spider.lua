acd2a701483b857f338d62d133bb9289bf0b57d7
Pantos <pantos+github@akk.org>
2025-03-07 14:46:22 +0100

    nvim: use nvim-spider only with WEB instead of replacing the default

--- dot_config/nvim/lua/plugins/spider.lua
+++ dot_config/nvim/lua/plugins/spider.lua
@@ -2,9 +2,9 @@
 return {
   "chrisgrieser/nvim-spider",
   config = function()
-    vim.keymap.set({ "n", "o", "x" }, "w", function() require("spider").motion "w" end, { desc = "Spider-w" })
-    vim.keymap.set({ "n", "o", "x" }, "e", function() require("spider").motion "e" end, { desc = "Spider-e" })
-    vim.keymap.set({ "n", "o", "x" }, "b", function() require("spider").motion "b" end, { desc = "Spider-b" })
-    vim.keymap.set({ "n", "o", "x" }, "q", function() require("spider").motion "b" end, { desc = "Spider-b" })
+    vim.keymap.set({ "n", "o", "x" }, "W", function() require("spider").motion "w" end, { desc = "Spider-w" })
+    vim.keymap.set({ "n", "o", "x" }, "E", function() require("spider").motion "e" end, { desc = "Spider-e" })
+    vim.keymap.set({ "n", "o", "x" }, "B", function() require("spider").motion "b" end, { desc = "Spider-b" })
+    vim.keymap.set({ "n", "o", "x" }, "Q", function() require("spider").motion "b" end, { desc = "Spider-b" })
   end,
 }
